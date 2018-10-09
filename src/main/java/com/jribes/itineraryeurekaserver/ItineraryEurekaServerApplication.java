16e0bcac6d30e72285c7dc3786b413e1938248f2
Jordi <jordi.ribes@fonyou.com>
2018-10-10 00:41:54 +0200

    Updating functionality

--- src/main/java/com/jribes/itineraryeurekaserver/ItineraryEurekaServerApplication.java
+++ src/main/java/com/jribes/itineraryeurekaserver/ItineraryEurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.jribes.itineraryeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class ItineraryEurekaServerApplication {
 
   public static void main(String[] args) {
